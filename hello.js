6e447352256f1602218c191af6d4627a7694ae0c
rgmeneses <rafa.meneses@gmail.com>
2024-05-14 19:55:09 -0600

    Cambio mensaje de hola mundo

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hola, Mundo, desde Mexico y Colombia !!!";
 console.log(hello());
 exports.hello = hello;
 
